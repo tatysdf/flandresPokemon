79ac4b4e8eee963e92e4957511cce2472eb7817c
tatysdf <djesstaty_655@icloud.com>
2025-04-25 09:38:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const path = require("path");
 const fs = require("fs");
 const app = express();
 
-const port = 5002;
+const port = process.env.PORT || 5002
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
